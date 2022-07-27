2ee17117da5fbb712bd6e84af90aace7d91cb5b9
lucaskenp <lucasmonteirolima17@gmail.com>
2022-07-27 20:11:23 -0300

    estrutura do site

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import Content from './components/Content';
 import GlobalStyles from './styles/GlobalStyles';
+import MenuSpacing from './components/MenuSpacing';
 
 function App() {
   return (
     <>
-      <Header />
       <GlobalStyles />
+      <Header />
+      <MenuSpacing />
       <Content />
     </>
   );
